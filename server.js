abfa17cae4c01c5c8a7206dab995489c9cda3ce7
gprohorov <yuramail222@gmail.com>
2020-10-29 12:41:53 +0200

    Requests OK

--- server.js
+++ server.js
@@ -5,14 +5,32 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import path from 'path';
 import mongoose from 'mongoose'
+// import cors from 'cors';
 
-console.log('Hello server')
+console.log('Hello server');
 
+const buildUrl = (version, path) => `api/${version}/${path}`
+const ITEMS_BASE_URL = '';
 const PORT = 8081;
 const server = express();
+const cors = require('cors');
+var corsOptions = {
+   origin: 'http://localhost:8081'
+}
+
+
 
 server.use(morgan('tiny'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended:true}));
+server.use(cors(corsOptions));
+
+server.get('/welcome', (req, res) =>{
+   res.json({message: "Welcome to .... "});
+    }
+);
 
 server.listen(PORT, () => {
    console.log(' Server started on port ' + PORT)
 });
+
