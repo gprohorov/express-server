566868825a03cf3ddb3d36cc9670e95ec4fd7342
gprohorov <yuramail222@gmail.com>
2020-12-03 11:26:14 +0200

    30 nov

--- controller/item.controller.js
+++ controller/item.controller.js
@@ -44,15 +44,47 @@ exports.create = (req, res) => {
 
 // Find a single Item with an id
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Item.findById(id)
+        .then(data => {
+            if (!data)
+                res.status(404).send({ message: "Not found Item with id " + id });
+            else res.send(data);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send({ message: "Error retrieving Item with id=" + id });
+        });
 };
 
 // Update a Item by the id in the request
 exports.update = (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
 
+    const id = req.params.id;
+
+    Item.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update Item with id=${id}. Maybe item was not found!`
+                });
+            } else res.send({ message: "Item was updated successfully." });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Item with id=" + id
+            });
+        });
 };
 
-// Delete a Item with the specified id in the request
+// Delete an Item with the specified id in the request
 exports.delete = (req, res) => {
 
     const id = req.params.id;
